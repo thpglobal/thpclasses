fafe06b29f2eefc37956a4406ca459a8900daddc
Murshed Ahmmad Khan <usamurai@gmail.com>
2018-10-04 18:45:04 +0600

    Sample menu items added for the demo

--- includes/menu_demo.php
+++ includes/menu_demo.php
@@ -1,3 +1,12 @@
 <?php
 // demo menu will go here - to use, rename to menu.php
+
+/* Sample Menu items */
+$menu=array("Home"=>"/",
+            "Sign Up"=>"/signup",
+            "Login"=>"/login",
+			      "History"=>"/history",
+			      "Data Entry"=>"/entry",
+            "Logout"=>"/logout"
+			);
 ?>
