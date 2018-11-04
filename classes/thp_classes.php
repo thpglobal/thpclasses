d394bf5bad51899196a17f2db518afa80d999ac4
The Hunger Project <john.coonrod@thp.org>
2018-11-04 13:51:16 -0500

    Update thp_classes.php

--- classes/thp_classes.php
+++ classes/thp_classes.php
@@ -780,7 +780,42 @@ class Chart{
 	public $stroke='#ACC26D';
 	public $point='#fff';
 	public $pointStroke='#9DB86D';
-	public function dark(){ // change color scheme for dashboards
+	public $options="scales:{xAxes:[{gridLines:{color:'yellow'}}],
+	yAxes:[{ticks:{beginAtZero:true},gridLines:{color:'yellow'}}]}\n");
+	public function start($color='white'){
+		echo("<script src=https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js></script>\n");
+		echo("<script>\n");
+		echo("Chart.defaults.global.responsive = true;\n");
+		echo("Chart.defaults.global.defaultColor = 'white';\n");
+		echo("Chart.defaults.global.defaultFontColor = 'white';\n");
+		echo("Chart.defaults.global.tooltips.backgroundColor = '#fff';\n");
+		echo("Chart.defaults.global.tooltips.titleFontColor = 'black';\n");
+		echo("Chart.defaults.global.tooltips.bodyFontColor = 'black';\n");
+		echo("Chart.defaults.global.animation.duration = 1500;\n");
+		echo("Chart.defaults.global.animation.easing = 'easeInOutQuart';\n");
+		echo("Chart.defaults.global.maintainAspectRatio = true;\n");
+		echo("Chart.defaults.global.legend.display = false;\n");
+		echo("var ChartOptions = {".$this->options."};\n</script>\n");
+	}
+	public function make($n,$ctitle,$ctype,$x,$y){
+		echo("<div class='pure-u-1-3'><h3>$ctitle</h3><canvas id=chart$n width=500 height=350></canvas></div>\n");
+		echo("<script>\n");
+		echo("var data$n = { \n");
+		echo("  labels : ".json_encode($x).",\n");
+		echo("  datasets : [{\n"); 
+		echo("	label: '$ctitle',\n");
+		if( 'line' == $ctype ){
+			echo("	fill: false,\n");
+    	}else{
+    		echo("	fill: true,\n");
+    	}
+		echo("	backgroundColor: 'lightgreen',\n");
+		echo("	borderWidth: 2,\n	borderColor: 'lightgreen',\n");
+		echo("	pointBorderColor: 'lightgreen',\n");
+		echo("	data: ".json_encode($y)."\n	} \n], \n}; \n");
+		echo("var c$n = document.getElementById('chart".$n."').getContext('2d');\n");
+ 		echo("var cc$n = new Chart(c$n,{ type: '$ctype', data: data$n, options: ChartOptions } );\n");
+ 		echo("</script>\n");
 	}
 	public function show($title="Sample",$type="Radar",$data=array("A"=>1,"B"=>2,"C"=>3)) {
 		if($this->ncharts==0) echo ("<script src=https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.1/Chart.min.js></script>\n");
