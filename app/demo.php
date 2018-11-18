442ae5fd610eb2cddfc6f5ea7787e30a571bc662
The Hunger Project <john.coonrod@thp.org>
2018-11-18 16:11:01 -0500

    Update demo.php

--- app/demo.php
+++ app/demo.php
@@ -4,9 +4,12 @@ $page=new Page;
 $page->icon("upload","upload","Import and excel spreadsheet");
 $page->icon("download","export","Export data as an excel spreadsheet");
 $page->start("Demo thp_classes");
-$grid=new Table:
+$grid=new Table;
 if(isset($_SESSION["contents"])) { $grid->contents=$_SESSION["contents"];}
 else{
+	$grid->header("Able","Baker","Charlie","Delta");
+	for($i=1;$i<100;$i++) $grid->row(array($i,$i+1;$i+2;$i+3));
 }
+$grid->show();
 $page->end();
 ?>
