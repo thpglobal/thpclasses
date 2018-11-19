52c6080a824508534d719655b8cfc2b95ed6b514
The Hunger Project <john.coonrod@thp.org>
2018-11-19 13:38:20 -0500

    Update and rename router.php to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Default router under php 7.2 on GAE
+$path=$_SERVER['REQUEST_URI'];
+$q=strpos($path,"?"); // strip off GET string if needed
+if($q) $path=substr($path,0,$q);
+if($path<>'/') {
+	include('app'.$path.'.php');
+}else{
+	include('app/demo.php');
+}
+?>
--- router.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path=$_SERVER['REQUEST_URI'];
-if(isset($_SERVER['PATH_INFO'])) {
-	include('app'.$path.'.php');
-}else{
-	include('app/demo.php');
-}
-?>
