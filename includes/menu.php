b7518b5e382ab68b09d198167d55221a9d6d973f
The Hunger Project <john.coonrod@thp.org>
2018-11-19 14:56:20 -0500

    Update and rename menu_demo.php to menu.php

---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+<?php
+// This is used only when the package is used standalone 
+
+/* Sample Menu items */
+$menu=array("Home"=>"/",
+            "Export"=>"export",
+            "Import"=>"upload",
+			"Query"=>"query",
+			"Session"=>"session",
+			"List"=>"list"
+			);
+?>
