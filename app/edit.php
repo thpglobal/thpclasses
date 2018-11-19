26aa63f8e84a0653c125b60f5bf41526e029f0d4
The Hunger Project <john.coonrod@thp.org>
2018-11-19 16:09:45 -0500

    Update edit.php

--- app/edit.php
+++ app/edit.php
@@ -1,11 +1,12 @@
 <?php // Edit one record from old budget tables - called from list1
-require_once(__DIR__."/../classes/thp_classes.php");
+require(__DIR__."/../classes/thp_classes.php"); // Load the classes
 $table=$_SESSION["table"];
 $id=$_SESSION["id"];
 if($id=='') $id=0;
 $prefix=($id>0 ? "Edit Record $id" : "Create new record");
 $page=new Page;
 $page->start("$prefix in $table");
+if($table=='') Die("No table set.");
 $form=new Form;
 $form->start($db,"/update");
 $form->record($table,$id);
