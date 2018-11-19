19a1e1566f53a27144106c8cf1c75eb0da1855dd
The Hunger Project <john.coonrod@thp.org>
2018-11-19 16:11:12 -0500

    Update list.php

--- app/list.php
+++ app/list.php
@@ -1,10 +1,11 @@
 <?php
 // SMART GENERIC LIST - OOP Version - Show the individual records from any report
-require_once('../classes/thp_classes.php');
+require(__DIR__."/../classes/thp_classes.php"); // Load the classes
 $table=$_SESSION["table"];
 $_SESSION["back"]="/list?table=$table";
 $page=new Page;
 if($can_edit) $page->icon("plus-circle","/edit?table=$table&id=0","Add new record");
+if($table=='') Die("No table set");
 $page->start($table);
 $grid=new Table;
 $grid->start($db);
