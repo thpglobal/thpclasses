3f596e77dfed7d585e9baeb88760565af8d80297
The Hunger Project <john.coonrod@thp.org>
2019-05-27 16:52:23 -0400

    Update query.php

--- app/query.php
+++ app/query.php
@@ -10,12 +10,17 @@ if( isset($_GET["query"]) ){
 	$query=$_GET["query"];
 	$start=substr($query,0,4);
 	if(in_array($start,array("show","sele","expl"))){
+		if(!strpos($query,'limit')) {
+			$query .= " limit 1000");
+			echo("<p>Note: Query limited to 1,000 rows</p>\n");
+		}
 		$grid=new Table;
 		$grid->start($db);
 		$grid->query($query);
 		$grid->show();
 	}else{
-		$db->exec($query);
+		$affected=$db->exec($query);
+		echo("<p>$affected rows affected</p>\n");
 	}
 }
 $page->end();
