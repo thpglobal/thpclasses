9c33864d8c6f419555a51ad45bf8caf1a829bd2f
The Hunger Project <john.coonrod@thp.org>
2019-05-30 12:04:50 -0400

    Removing Errors form from pages

--- classes/page.php
+++ classes/page.php
@@ -135,7 +135,6 @@ $(document).ready(function() {
 	public function end($message=""){
 		$time=microtime(true)-($this->time_start);
 		echo("<p><i>$message Run time: $time</i></p>\n");
-		echo("<p><a href='https://forms.gle/FB5pkmjGoqG3DpRC9' target='_blank'> Please fill out this form if you are experiencing any functional issues.</a></p>");
 		echo("</div>\n");
         echo("</body></html>\n");
     }
