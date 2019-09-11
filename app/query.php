e7bab16d278a8a8303bb440186a0bf3f882247eb
The Hunger Project <john.coonrod@thp.org>
2019-09-11 16:21:35 -0400

    Update query.php

--- app/query.php
+++ app/query.php
@@ -1,27 +1,72 @@
 <?php
 // Simple query display
-require(__DIR__."/../classes/thp_classes.php"); // Load the classes
+require_once($_SERVER["DOCUMENT_ROOT"]."/thpclasses/classes/thp_classes.php");
 $page=new Page;
 $page->icon("download","/export","Download as excel");
+$page->icon("pie-chart","chart?chart_type=pie","Display as Pie Chart");
+$page->icon("bar-chart","chart?chart_type=bar","Display as Bar Chart");
+$page->icon("line-chart","chart?chart_type=line","Display as Line Chart");
 $page->start("Query");
-if(!$admin) Die("Not authorized.");
-echo("<form><textarea name=query rows=3 cols=80>".$_GET["query"]."</textarea><input type=submit></form>\n");
+?>
+<p><a class=pure-button href=/admin/status>Show tables</a>
+<button class=pure-button onclick="goback()">&lt;</button>
+<button class=pure-button onclick="gofwd()">&gt;</button>
+<button class=pure-button onclick="goclear()">&#x1f5d1;</button> 
+<span id=nq>0</span> queries stored. Loading <span id=iq>0</span>.</p>
+<?php
+echo("<form><textarea id=q name=query rows=3 cols=80>".$_GET["query"]."</textarea><input type=submit onclick='saveq()'></form>\n");
 if( isset($_GET["query"]) ){
 	$query=$_GET["query"];
 	$start=substr($query,0,4);
 	if(in_array($start,array("show","sele","expl"))){
-		if($start=='sele' and !strpos($query,'limit')) {
-			$query .= " limit 1000";
-			echo("<p>Note: Query limited to 1,000 rows</p>\n");
-		}
 		$grid=new Table;
 		$grid->start($db);
 		$grid->query($query);
 		$grid->show();
 	}else{
-		$affected=$db->exec($query);
-		echo("<p>$affected rows affected</p>\n");
+		$db->exec($query);
 	}
 }
-$page->end();
 ?>
+<!-- The following are used to store past copies of the query in local storage -->
+<script>
+  var queries = [];
+  if (!localStorage.queries)
+    window.localStorage.setItem('queries',JSON.stringify(queries));
+  queries=JSON.parse(window.localStorage.getItem('queries'));
+  var n=queries.length;
+  var i=n;
+  showni();
+
+function saveq(){
+  queries[n]=document.getElementById('q').value;
+  window.localStorage.setItem('queries',JSON.stringify(queries));
+}
+function goback(){
+  i=i-1;
+  if(i<0) i=0;
+  document.getElementById('q').textContent=queries[i];
+  showni();
+}
+function gofwd(){
+  i=i+1;
+  if(i>(n-1)) i=n-1;
+  if(i<0) i=0;
+  document.getElementById('q').textContent=queries[i];
+  showni();
+}
+function goclear(){
+  window.localStorage.clear();
+  queries=[];
+  n=0;
+  i=0;
+  document.getElementById('q').textContent='';
+  showni();
+}
+function showni(){
+  document.getElementById('nq').textContent=n;
+  document.getElementById('iq').textContent=i;
+}
+</script>
+<?php 
+$page->end();
