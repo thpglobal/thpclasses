976a366d1bb94c543c23d7c5f7cb811f2186bb9e
The Hunger Project <john.coonrod@thp.org>
2020-05-08 16:06:09 -0400

    Update login2.php

--- app/login2.php
+++ app/login2.php
@@ -1,8 +1,10 @@
 <?php
+$next_path=$_COOKIE["path"];
+if(!$next_path) $next_path="/";
 $checkendpoint = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=";
 $checkurl= $checkendpoint.$_POST['idtoken'];
 $v=file_get_contents($checkurl);
 $vp=json_decode($v,true);
 $name=$vp["name"]; $_SESSION["name"]=$name;
 $user=$vp["email"]; $_SESSION["user"]=$user;
-header("Location:/");
+header("Location:".$next_path);
