e29c97dd05691236b4f7f1293ecd8207fcfea990
The Hunger Project <john.coonrod@thp.org>
2020-05-08 14:32:17 -0400

    Update page.php

--- classes/page.php
+++ classes/page.php
@@ -146,7 +146,7 @@ $(document).ready(function() {
 		echo("<div id='google_translate_element' style='position:absolute; top:4em; right:1em;'></div> 
 			<script type='text/javascript'> 
 				function googleTranslateElementInit() { 
-					new google.translate.TranslateElement({pageLanguage: 'bn'}, 'google_translate_element'); 
+					new google.translate.TranslateElement({pageLanguage: '$lang'}, 'google_translate_element'); 
 				} 
 			</script> 
 		<script type='text/javascript' src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'></script>");
