5de9586998784950f09b66ece19734f718da7d6d
The Hunger Project <john.coonrod@thp.org>
2020-06-01 10:37:16 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,6 +2,4 @@
 require(__DIR__."/../classes/page.php");
 $page=new Page;
 $page->start("THP Classes Test Platform!");
-$driver=$db->getAttribute(PDO::ATTR_DRIVER_NAME);
-echo("<p>You are connected to a $driver database.</p>\n");
 $page->end();
