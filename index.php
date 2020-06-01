c2d8158794d05e5aca029aa1d9ca0442dccb0604
The Hunger Project <john.coonrod@thp.org>
2020-05-31 21:01:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-// Default router under php 7.2 on GAE
+// Default router under php 7+
+session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL & ~E_NOTICE);
+
 $path=$_SERVER['REQUEST_URI'];
 $path=parse_url($url, PHP_URL_PATH);
 if($path<>'/') {
