25e35289c6135ff6c1c0b11b5e55a56237f5f276
The Hunger Project <john.coonrod@thp.org>
2020-05-31 21:40:19 -0400

    Update cookies.php

--- app/cookies.php
+++ app/cookies.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER["DOCUMENT_ROOT"]."/thpclasses/classes/thp_classes.php");
+require_once(__DIR__."/../classes/thp_classes.php");
 $page=new Page;
 $page->start("Cookies");
 echo("<pre>".print_r($_COOKIE,TRUE)."</pre>");
