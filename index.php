7debc67dc4195e4fda79621a3f5eb33af2ef3761
The Hunger Project <john.coonrod@thp.org>
2020-05-31 21:06:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL & ~E_NOTICE);
-
+$root=$_SERVER["DOCUMENT_ROOT"];
 $path=$_SERVER['REQUEST_URI'];
+console.log($path);
 $path=parse_url($url, PHP_URL_PATH);
 if($path<>'/') {
-	include('app'.$path.'.php');
+	include($root.'app'.$path.'.php');
 }else{
 	include('app/index.php');
 }
