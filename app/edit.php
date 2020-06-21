437d8d0713225702148dfa9671dc8cc2fd99b960
The Hunger Project <john.coonrod@thp.org>
2020-06-21 09:41:44 -0400

    Update edit.php

--- app/edit.php
+++ app/edit.php
@@ -1,7 +1,15 @@
 <?php // Edit one record from old budget tables
+// 2020-06 allow the ability to pass one hidden identifier name to be preset by cookie value
 require(__DIR__."/../classes/thp_classes.php"); // Load the classes
 $table=$_COOKIE["table"];
 $id=$_COOKIE["id"];
+$hide=$_COOKIE["hidden"]; // Allow one hidden variable to be passed in the url
+if($hide) {
+	// is it the name of a dropdown link?
+	$n=strpos("_ID",$hide); 
+	$cookie_name= strtolower( ($n ? substr($hide,0,$n-1) : $hide));
+	$hidden=array($hide=>$_COOKIE[$cookie_name]);
+}													
 if($id=='') $id=0;
 $prefix=($id>0 ? "Edit Record $id" : "Create new record");
 $page=new Page;
